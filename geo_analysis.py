e49b9479af385af5e55eb4f4cb76a347c13b63df
giulio-sido <58228913+giulio-sido@users.noreply.github.com>
2021-04-29 16:19:14 +0200

    geo chunks analysis

--- geo_analysis.py
+++ geo_analysis.py
@@ -1,7 +1,17 @@
 import pandas as pd
 import json
+from itertools import islice
 
 RATIO = 0.7
+MIN_SIZE = 1000
+BATCH_SIZE = 100
+
+
+
+def chunks(data, SIZE=BATCH_SIZE):
+    it = iter(data)
+    for i in range(0, len(data), SIZE):
+        yield {k:data[k] for k in islice(it, SIZE)}
 
 
 # prepare dictionary with geo data
@@ -75,7 +85,34 @@ for continent, row in continents.items():
         if row["count"] != 0 and column != "count":
             continents[continent][column] = data / row["count"]
 
-
-        
-
 continent_df = pd.DataFrame.from_dict(continents, orient="index").drop(["AN", "MANY"]).drop(columns=["avg_shortest_path_len"])
+# print(continent_df)
+
+# continent stats ordered by number of nodes
+
+sorted_geo_all_dict = {k: v for k, v in sorted(geo_all_dict.items(), key=lambda item: item[1]["tot_nodes_count"]) if v["tot_nodes_count"] >= MIN_SIZE}
+
+
+ratio_dict = dict()
+for as_number, data in sorted_geo_all_dict.items():
+    tot = data["tot_nodes_count"]
+    ratio_dict[as_number] = max(data["NA_nodes_count"], data["SA_nodes_count"], data["EU_nodes_count"], data["AF_nodes_count"], data["AS_nodes_count"], data["OC_nodes_count"], data["AN_nodes_count"]) / tot
+# print(ratio_dict)
+
+chunks_data = dict()
+i = 0
+for chunk in chunks(ratio_dict, 30):
+    count = 0
+    for as_number, value in chunk.items():
+        if value < RATIO:
+            count += 1
+    chunks_data[i] = {
+        "multi_continent_AS_count": count,
+        "largest_AS_nodes_count": sorted_geo_all_dict[as_number]["tot_nodes_count"]
+    }
+    i += 1
+
+chunks_df = pd.DataFrame.from_dict(chunks_data, orient="index")
+with open("stats/geo_chunks.csv", "w", encoding="utf8") as out_file:
+    chunks_df.to_csv(out_file)
+print(chunks_df)
